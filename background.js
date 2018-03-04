5fd794157d6a9dbabcee06b95fc323adb5f8b91b
Juang, Yi-Lin <frankyjuang@gmail.com>
2018-03-04 22:41:15 +0800

    Lint using eslint and google js style guide

--- background.js
+++ background.js
@@ -2,19 +2,20 @@
 
 // TODO: right-click edit/remove rule, rule page
 
-let currentTabId = "";  // Current tab id of activated tab.
-let currentBmId = "";   // Current bookmark id of activated tab.
-let newTracking = {};   // Information for tracking new series including `windowId` and `url`.
+let currentTabId = ""; // Current tab id of activated tab.
+let currentBmId = ""; // Current bookmark id of activated tab.
+let newTracking = {}; // Tracking new series. `windowId` and `url`.
 
-chrome.windows.onFocusChanged.addListener(windowId => {
-    if ((!newTracking.windowId && newTracking.url) || windowId === newTracking.windowId) {
+chrome.windows.onFocusChanged.addListener((windowId) => {
+    if ((!newTracking.windowId && newTracking.url) ||
+            windowId === newTracking.windowId) {
         halfableIcon();
     } else {
         chrome.tabs.query({
             active: true,
-            windowId: windowId
-        }, async tabs => {
-            if (tabs.length > 1) {  // Current at abnormal window.
+            windowId: windowId,
+        }, async function(tabs) {
+            if (tabs.length > 1) { // Current at abnormal window.
                 return;
             }
             currentTabId = tabs[0].id;
@@ -24,7 +25,7 @@ chrome.windows.onFocusChanged.addListener(windowId => {
 });
 
 
-chrome.windows.onRemoved.addListener(windowId => {
+chrome.windows.onRemoved.addListener((windowId) => {
     if (windowId === newTracking.windowId) {
         newTracking = {};
     }
@@ -38,7 +39,7 @@ chrome.tabs.onActivated.addListener(async function(info) {
 
 chrome.tabs.onUpdated.addListener(async function(tabId, info, tab) {
     if (tab.windowId === newTracking.windowId) return;
-    if (tabId === currentTabId) {  // Activated tab.
+    if (tabId === currentTabId) { // Activated tab.
         currentBmId = await resolveBmId(currentTabId);
     }
 });
@@ -49,8 +50,8 @@ chrome.storage.onChanged.addListener((changes, area) => {
 });
 
 function resolveBmExistence(url, bmId) {
-    return new Promise(resolve => {
-        chrome.bookmarks.get(bmId, nodes => {
+    return new Promise((resolve) => {
+        chrome.bookmarks.get(bmId, (nodes) => {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message);
                 resolve(false);
@@ -60,7 +61,9 @@ function resolveBmExistence(url, bmId) {
                 resolve(false);
                 return;
             }
-            if (nodes[0].url.toLowerCase() !== url) {  // New episode ready for update.
+
+            // New episode ready for update.
+            if (nodes[0].url.toLowerCase() !== url) {
                 chrome.browserAction.setBadgeText({text: " "});
             }
             resolve(true);
@@ -69,22 +72,23 @@ function resolveBmExistence(url, bmId) {
 }
 
 function resolveBmId(tabId) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         disableIcon();
         chrome.browserAction.setBadgeText({text: ""});
-        chrome.tabs.get(tabId, tab => {
+        chrome.tabs.get(tabId, (tab) => {
             console.debug(tab.url);
             let url = tab.url.toLowerCase();
             chrome.storage.local.get(null, async function(items) {
                 for (let [r, bmId] of Object.entries(items)) {
-                    let re = new RegExp(r.substring(1, r.length-1));  // Deserialize regexp object.
-                    if (re.test(url)) {  // Regexp matches the url (this page).
+                    // Deserialize regexp object.
+                    let re = new RegExp(r.substring(1, r.length-1));
+                    if (re.test(url)) { // Regexp matches the url (this page).
                         if (await resolveBmExistence(url, bmId)) {
                             resolve(bmId);
                             enableIcon();
                             return;
-                        } else {  // Bookmark of this rule was removed.
-                            chrome.storage.local.remove(r);  // Remove this rule.
+                        } else { // Bookmark of this rule was removed.
+                            chrome.storage.local.remove(r); // Remove this rule.
                         }
                     }
                 }
@@ -94,11 +98,11 @@ function resolveBmId(tabId) {
     });
 }
 
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.browserAction.onClicked.addListener((tab) => {
     if (tab.url.toLowerCase().startsWith("chrome://")) {
         return;
     }
-    if (tab.windowId === newTracking.windowId) {  // Tracking window.
+    if (tab.windowId === newTracking.windowId) { // Tracking window.
         let url = tab.url.toLowerCase();
         let ss = getSharedStart(url, newTracking.url);
 
@@ -107,34 +111,39 @@ chrome.browserAction.onClicked.addListener(tab => {
         if (re.test(url)) {
             chrome.bookmarks.create({
                 title: tab.title,
-                url: tab.url
-            }, node => {
+                url: tab.url,
+            }, (node) => {
                 let item = {};
                 item[re] = node.id;
                 chrome.storage.local.set(item);
             });
         } else {
-            alert("Track failed")
+            alert("Track failed");
         }
 
         chrome.windows.remove(newTracking.windowId);
         newTracking = {};
         return;
     }
-    if (currentBmId === "") {  // Untracked series.
+    if (currentBmId === "") { // Untracked series.
         newTracking.url = tab.url.toLowerCase();
         chrome.windows.create({
             url: tab.url,
             focused: true,
-            type: "normal"  // "popup"
-        }, w => { newTracking.windowId = w.id; });
-        alert("Open another episode in this window and click Epimarks again to confirm.\nHint: open same-season episode for season tracking. open different-season same-series episode for series tracking.");
-    } else {  // Tracked series.
-        console.debug("Bookmark " + currentBmId + " is updated")
+            type: "normal", // "popup"
+        }, (w) => {
+            newTracking.windowId = w.id;
+        });
+        alert("Open another episode in this window and click Epimarks again " +
+            "to confirm.\n\nHint:\nOpen same-season episode for season" +
+            "tracking.\nOpen different-season same-series episode for series" +
+            "tracking.");
+    } else { // Tracked series.
+        console.debug("Bookmark " + currentBmId + " is updated");
         chrome.browserAction.setBadgeText({text: ""});
         chrome.bookmarks.update(currentBmId, {
             title: tab.title,
-            url: tab.url
+            url: tab.url,
         });
     }
 });
@@ -143,7 +152,7 @@ function getSharedStart(s1, s2) {
     let i = 0;
     while (i < s1.length && s1.charAt(i) === s2.charAt(i)) i++;
     return s1.substring(0, i);
-};
+}
 
 function enableIcon() {
     chrome.browserAction.setIcon({
@@ -151,10 +160,10 @@ function enableIcon() {
             "16": "icons/bookmark16.png",
             "24": "icons/bookmark24.png",
             "32": "icons/bookmark32.png",
-            "64": "icons/bookmark64.png"
-        }
+            "64": "icons/bookmark64.png",
+        },
     });
-};
+}
 
 function halfableIcon() {
     chrome.browserAction.setIcon({
@@ -162,10 +171,10 @@ function halfableIcon() {
             "16": "icons/halfbookmark16.png",
             "24": "icons/halfbookmark24.png",
             "32": "icons/halfbookmark32.png",
-            "64": "icons/halfbookmark64.png"
-        }
+            "64": "icons/halfbookmark64.png",
+        },
     });
-};
+}
 
 function disableIcon() {
     chrome.browserAction.setIcon({
@@ -173,7 +182,7 @@ function disableIcon() {
             "16": "icons/unbookmark16.png",
             "24": "icons/unbookmark24.png",
             "32": "icons/unbookmark32.png",
-            "64": "icons/unbookmark64.png"
-        }
+            "64": "icons/unbookmark64.png",
+        },
     });
-};
+}
